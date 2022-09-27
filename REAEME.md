cdbe4ca6702e6d48eb1858983b53bf96a990a977
belongtothenight <dachuan516@gmail.com>
2022-09-27 20:28:18 +0800

    initialize

---
+++ REAEME.md
@@ -0,0 +1,15 @@
+# DIP_Code
+
+![Repo Size](https://img.shields.io/github/repo-size/belongtothenight/DIP_Code) ![Code Size](https://img.shields.io/github/languages/code-size/belongtothenight/DIP_Code) ![File Count](https://img.shields.io/github/directory-file-count/belongtothenight/DIP_Code/src) ![Commit Per Month](https://img.shields.io/github/commit-activity/m/belongtothenight/DIP_Code)
+
+This repo contains all the codes of Digital Image Processing class and have description on the wiki tab.
+
+## Notice
+
+Scripts in this repo needs to be executed with MATLAB. Adjustment is needed for GNU Octave.
+
+## File Description
+
+| No. | Filename   | Description                                                                                |
+| :-: | ---------- | ------------------------------------------------------------------------------------------ |
+|  1  | draw3pic.m | Draw three identical square except for their gray-scale pixel value. Illustrates illution. |
---
+++ src/draw3pic.m
@@ -0,0 +1,28 @@
+function draw3pic
+  % ====================================================================================
+  % Since the picture is regulated by pixel size, it is unwise to just plot in XY plane.
+  % rectangle ("Position", [0.05, 0.05, 0.9, 0.9]);
+  % title ("rectangle() with corners curved");
+  % ============Function=======
+  function image = create_function(ol, il, ov, iv)
+    % ol:  outer length
+    % il:  inner length
+    % ov: outer value
+    % iv: inner value
+    %l = round(ol/2)+round(il/2)
+    %s = round(ol/2)-round(il/2)
+    s = round(ol/2-il/2)+1;
+    l = round(ol/2+il/2);
+    array = ones(ol, ol)*ov;
+    array(s:l,s:l) = array(s:l,s:l)/ov*iv;
+    image = mat2gray(array, [0 255]);
+  end
+  % ============Array==========
+  img{1} = create_function(128, 40, 50, 150);
+  img{2} = create_function(128, 40, 100, 150);
+  img{3} = create_function(128, 40, 200, 150);
+  
+  % ============Main===========
+  space = mat2gray((ones(128, 5)*255), [0 255]);
+  montage([img{1} space img{2} space img{3}], 'ThumbnailSize', [])
+end
