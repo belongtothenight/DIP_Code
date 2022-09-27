77b55572ea87106257b63591663924259dcf39b5
belongtothenight <dachuan516@gmail.com>
2022-09-27 22:06:55 +0800

    Update draw3pic.m

--- src/draw3pic.m
+++ src/draw3pic.m
@@ -3,12 +3,10 @@ function draw3pic
   % Since the picture is regulated by pixel size, it is unwise to just plot in XY plane.
   % ============Function=======
   function image = create_function(ol, il, ov, iv)
-    % ol:  outer length
-    % il:  inner length
+    % ol: outer length
+    % il: inner length
     % ov: outer value
     % iv: inner value
-    %l = round(ol/2)+round(il/2)
-    %s = round(ol/2)-round(il/2)
     s = round(ol/2-il/2)+1;
     l = round(ol/2+il/2);
     array = ones(ol, ol)*ov;
